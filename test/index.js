08f3adf62cfd009cf0e933a58039628d84bf60ab
npvinhthai <npvinhthai@gmail.com>
2017-03-21 11:46:45 +0700

    update linebreaks Readme

--- test/index.js
+++ test/index.js
@@ -9,10 +9,10 @@ langPacks.vi = new langPackage();
 langPacks.en = new langPackage();
 langPacks.vi.importJSON('./test/vi/default');
 
-console.dir(langPacks.vi.get('HELLO', {balance: '10,000', lastLogin: '30/01/2017',username: 'Trung'}));
+
 describe('#hello', function() {
     it('translate hello', function () {
-        langPacks.vi.get('HELLO', {balance: '10,000', lastLogin: '30/01/2017',username: 'Trung'}).should.equal("Xin chào Trung. Số dư của bạn là: 10,000. Login lần cuối ngày: 30/01/2017");
+        langPacks.vi.get('HELLO', {lastLogin: '30/01/2017',username: 'Trung'}).should.equal("Hello Trung. Your last login on: 30/01/2017");
     });
 });
 
